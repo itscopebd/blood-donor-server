06a4ad899b3f7457c8dc10c7c4bfdba09b63fd9a
itscopebd <itscopebd@gmail.com>
2023-06-28 07:18:40 -0700

    user insert api create

--- index.js
+++ index.js
@@ -1,2 +1,65 @@
-const express= require("express");
+const express = require("express");
 const app = express();
+require('dotenv').config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+
+// middle ware 
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Ropirq")
+})
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.loltiyt.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const users = client.db("blood-donor").collection("users");
+
+        app.post("/users", async (req, res) => {
+            const data = req.body;
+            const result = await users.insertOne(data);
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+        // Send a ping to confirm a successful connection
+        client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port, () => {
+    console.log("Running......")
+})
